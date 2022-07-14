d20a6e6c3e7b201c65cccf4e20eafbf2fbb20c58
robel baye <44139915+Amares03@users.noreply.github.com>
2022-07-15 02:32:23 +0300

    title icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>GFMP</title>
+    <!-- title Icon -->
+        <link rel = "icon" href = "/assets/logo.png" type = "image/x-icon">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
